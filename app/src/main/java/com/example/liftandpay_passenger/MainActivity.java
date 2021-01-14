1a0154d577f3d8111b40476b4ab38998dad4f071
Kwesi Ansu <kwesiansu16@gmail.com>
2021-01-14 02:07:33 +0000

    The Bottom navigation bar

--- app/src/main/java/com/example/liftandpay_passenger/MainActivity.java
+++ app/src/main/java/com/example/liftandpay_passenger/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.liftandpay_passenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        
     }
 
     
