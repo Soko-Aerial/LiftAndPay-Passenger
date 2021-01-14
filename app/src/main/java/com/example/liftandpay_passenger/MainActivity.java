86fbc7300872d9688ece19c4a47335e9d4d43da8
Kwesi Ansu <kwesiansu16@gmail.com>
2021-01-14 21:43:30 +0000

    The Bottom navigation bar one

--- app/src/main/java/com/example/liftandpay_passenger/MainActivity.java
+++ app/src/main/java/com/example/liftandpay_passenger/MainActivity.java
@@ -1,20 +1,50 @@
 package com.example.liftandpay_passenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        
+
+
+
+
+
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottom_navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId())
+            {
+                case R.id.main:
+                     fragment = new Fragment();
+                    break;
+
+              //  case R.id.pa
+
+            }
+
+            return false;
+        }
+    };
 
     
 }
