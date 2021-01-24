7671e86e5a0e74445f85662a72f3add8634bd3a0
Kwesi Ansu <kwesiansu16@gmail.com>
2021-01-24 18:54:02 +0000

    Recycler view added to rides history fragment. Adding recycler view to other fragments but when text added below recycler view it doesnt show

--- app/src/main/java/com/example/liftandpay_passenger/RidesFragment.java
+++ app/src/main/java/com/example/liftandpay_passenger/RidesFragment.java
@@ -3,14 +3,18 @@ package com.example.liftandpay_passenger;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RidesFragment#newInstance} factory method to
+
  * create an instance of this fragment.
  */
 public class RidesFragment extends Fragment {
@@ -23,6 +27,10 @@ public class RidesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    RecyclerView recyclerView;
+    RecyclerView recView;
+    ArrayList<datamodel> dataholder;
+
 
     public RidesFragment() {
         // Required empty public constructor
@@ -34,7 +42,7 @@ public class RidesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RidesFragment.
+     * @return A new instance of fragment dataFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static RidesFragment newInstance(String param1, String param2) {
@@ -59,6 +67,44 @@ public class RidesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rides, container, false);
+        View view =  inflater.inflate(R.layout.fragment_rides, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        dataholder = new ArrayList<>();
+        datamodel ob1 = new datamodel(R.drawable.img_map,"Toyota Auris","08.09.2018 9:59 PM","20km","$3.00");
+        dataholder.add(ob1);
+
+        datamodel ob2 = new datamodel( R.drawable.img_map,"Kia Creed","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob2);
+
+        datamodel ob3 = new datamodel( R.drawable.img_map,"Ford Fiesta","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob3);
+
+        datamodel ob4 = new datamodel( R.drawable.img_map,"Ford Fiesta","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob4);
+
+        datamodel ob5 = new datamodel( R.drawable.img_map,"Ford Fiesta","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob5);
+
+        recyclerView.setAdapter(new myadapter(dataholder));
+
+        datamodel ob6 = new datamodel( R.drawable.img_map,"Ford","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob6);
+
+        recyclerView.setAdapter(new myadapter(dataholder));
+
+        datamodel ob7 = new datamodel( R.drawable.img_map,"Audi","08.09.2018 9:59 PM","30km","$5.00");
+        dataholder.add(ob7);
+
+
+
+        recyclerView.setAdapter(new myadapter(dataholder));
+
+        /*
+        recView = view.findViewById(R.id.recview);
+        recView.setLayoutManager(new LinearLayoutManager(getContext()));
+*/
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/liftandpay_passenger/datamodel.java
@@ -0,0 +1,60 @@
+package com.example.liftandpay_passenger;
+public class datamodel {
+    int image;
+    String header,desc,distance,price;
+//data for ride history for the recycler view
+
+    public datamodel(int image, String header, String desc, String distance, String price) {
+        this.image = image;
+        this.header = header;
+        this.desc = desc;
+        this.distance = distance;
+        this.price =price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+
+
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
