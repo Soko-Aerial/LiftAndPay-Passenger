20a6f3c31b6fd7c8b96d71061acb879edcb0781c
kwesi <kwesiansu16@gmail.com>
2021-03-19 03:55:24 -0700

    adding phone auth 1

--- app/src/main/java/com/example/liftandpay_passenger/BottomSheetDialog.java
+++ app/src/main/java/com/example/liftandpay_passenger/BottomSheetDialog.java
@@ -14,9 +14,14 @@ import java.util.ArrayList;
 public class BottomSheetDialog extends BottomSheetDialogFragment {
     ArrayList<carBookingModel> carholder;
 
+
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bottomsheetlayout, container, false);
 
         return v;
+
+
     }
+
 }
