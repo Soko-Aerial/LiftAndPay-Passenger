3cddd2de4d6338e5339a03fa5ee16a9f122cfce3
hubamp <hubamp@gmail.com>
2021-05-03 18:34:04 +0000

    Retrieve Available rides AVR dialog fragment created AVR dialog linked to car details. cleared redundant dependencies

--- app/src/main/java/com/example/liftandpay_passenger/AvailableRideDialog.java
+++ app/src/main/java/com/example/liftandpay_passenger/AvailableRideDialog.java
@@ -16,16 +16,26 @@ import java.util.ArrayList;
 public class AvailableRideDialog extends BottomSheetDialogFragment {
 
     TextView carDetails;
+    TextView checkMap;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.available_ride_dialog, container, false);
 
         carDetails = v.findViewById(R.id.carDetails_avrId);
+        checkMap = v.findViewById(R.id.checkMap_avrId);
 
         carDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(getContext(),CarDetailsActivity_avr.class);
+                startActivity(intent);
+            }
+        });
+
+        checkMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getContext(),CheckMapActivity.class);
                 startActivity(intent);
             }
         });
